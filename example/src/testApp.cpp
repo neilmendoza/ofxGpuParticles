df9f9deb68c13597ceae4191fd7b4300b6064977
Neil Mendoza <neil@isthis.gd>
2014-01-04 00:15:07 +0000

    adding example

---
+++ example/src/testApp.cpp
@@ -0,0 +1,109 @@
+#include "testApp.h"
+
+//--------------------------------------------------------------
+void testApp::setup()
+{
+    ofBackground(0);
+    ofSetFrameRate(60);
+    
+    // 4,000,000 particles
+    unsigned w = 2000;
+    unsigned h = 2000;
+    
+    particles.init(w, h);
+    
+    // initial positions
+    // use new to allocate 16,000,000 floats on the heap rather than
+    // the stack
+    float* particlePosns = new float[w * h * 4];
+    for (unsigned y = 0; y < h; ++y)
+    {
+        for (unsigned x = 0; x < w; ++x)
+        {
+            unsigned idx = y * w + x;
+            particlePosns[idx * 4] = 400.f * x / (float)w - 200.f; // particle x
+            particlePosns[idx * 4 + 1] = 400.f * y / (float)h - 200.f; // particle y
+            particlePosns[idx * 4 + 2] = 0.f; // particle z
+            particlePosns[idx * 4 + 3] = 0.f; // dummy
+        }
+    }
+    particles.loadDataTexture(ofxGpuParticles::POSITION, particlePosns);
+    delete[] particlePosns;
+    
+    // initial velocities
+    particles.zeroDataTexture(ofxGpuParticles::VELOCITY);
+    
+    // listen for update event to set additonal update uniforms
+    ofAddListener(particles.updateEvent, this, &testApp::onParticlesUpdate);
+}
+
+//--------------------------------------------------------------
+void testApp::update()
+{
+    ofSetWindowTitle(ofToString(ofGetFrameRate(), 2));
+    particles.update();
+}
+
+// set any update uniforms in this function
+void testApp::onParticlesUpdate(ofShader& shader)
+{
+    ofVec3f mouse(ofGetMouseX() - .5f * ofGetWidth(), .5f * ofGetHeight() - ofGetMouseY() , 0.f);
+    shader.setUniform3fv("mouse", mouse.getPtr());
+    shader.setUniform1f("elapsed", ofGetLastFrameTime());
+    shader.setUniform1f("radiusSquared", 200.f * 200.f);
+}
+
+//--------------------------------------------------------------
+void testApp::draw()
+{
+    cam.begin();
+    ofEnableBlendMode(OF_BLENDMODE_ADD);
+    particles.draw();
+    ofDisableBlendMode();
+    cam.end();
+}
+
+//--------------------------------------------------------------
+void testApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void testApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void testApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void testApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
